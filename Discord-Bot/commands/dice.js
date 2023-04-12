8d40b5f3b03279b2bd5dae3a2cf3a6f5b8e6b4e8
Danne69420 <Daniel.kindvall05@gmail.com>
2023-04-12 16:45:47 +0200

    Add ability to add bonus to rolls
    This is an absolutely terrible way of doing this, but I am bad at javascript.

--- Discord-Bot/commands/dice.js
+++ Discord-Bot/commands/dice.js
@@ -1,9 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+//this might be the ugliest code i have ever writtien, but it (mostly) works. 
 var dice = {
-    roll: function (diceArgs) {
+    roll: function (input) {
+
+      //split at d to seperate number of sides from number of dice
+      const diceArgs = input.split("d");
+      //split at + to seperate additional bonuses
+      const plusArg = diceArgs[1].split("+");
+
+      console.log(plusArg);
+      console.log(diceArgs);
+
+      if(diceArgs[1].includes("+")){
+        diceArgs[1] = diceArgs[1].replace("+"+ plusArg[1], "");
+      }
+
       //if input is in correct the format (XdX) the array will always have 2 elements. ToDo: somehow check if input format is correct before command is sent. 
       if (diceArgs.length == 2){
+
         var sides = diceArgs[1];
         var numberOfDice = diceArgs[0];
         var totalResult = 0;
@@ -16,6 +30,10 @@ var dice = {
           var randomNumber = Math.floor(Math.random() * sides) + 1;
           totalResult += randomNumber;
           individualResults[i] = randomNumber;
+        }
+        //If there is a bonus we add it to the total
+        if(plusArg.length != 1){
+          totalResult += parseInt(plusArg[1]);        
         }
         const returnString = new String('Individual dice results: ' + individualResults.join(" ") + '\n Total: ' + totalResult);
         return returnString;
@@ -36,12 +54,11 @@ module.exports = {
         .setRequired(true)),
 	async execute(interaction) {
     var input = interaction.options.getString('input');
-    const diceArgs = input.split("d");
 
     await interaction.reply("rolling...");
     await setTimeout(editReply, 2000);
     function editReply(){
-      interaction.editReply(dice.roll(diceArgs).toString());
+      interaction.editReply(dice.roll(input).toString());
     }
 },
 };
