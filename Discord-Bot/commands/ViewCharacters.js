73e16acea7dd13e905adb064f128d745bbd5b7c1
Danne69420 <Daniel.kindvall05@gmail.com>
2023-05-18 09:52:53 +0200

    Update ViewCharacters.js
    still doesn't work as intended

--- Discord-Bot/commands/ViewCharacters.js
+++ Discord-Bot/commands/ViewCharacters.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
-
+//this command doesn't really work properly. savedcharacters.json is probably not going to work. Maybe just have it try to load all the files in ./PDFs.
 module.exports ={
     data: new SlashCommandBuilder()
         .setName('view-all-characters')
-        .setDescription("displays all saved characters"),
+        .setDescription("displays all saved characters and their stats"),
 
     async execute(interaction) {
         fs.readFile("./PDFs/savedCharacters.json", "utf-8", (err, jsonString) => {
@@ -16,7 +16,19 @@ module.exports ={
             characterArray = savedCharacters.split(",")
             for(let i = 0; i < characterArray.length; i++){
                 fs.readFile("./PDFs/" + characterArray[i], "utf-8", (err, jsonString) =>{
-
+                    if(err){
+                        console.log("error reading json file: ", err);
+                        return;
+                    }
+                    const character = JSON.parse(jsonString);
+                    let replyString = "";
+                    for(let i = 0; i < character.length; i++){
+                        if(character[i].type === 'alpha' && character[i].value != ''){
+                            console.log(character[i].id + ": " + character[i].value);
+                            replyString += "\n" + character[i].id + ": " + character[i].value;
+                        }
+                    }
+        
                 })
             }
 
